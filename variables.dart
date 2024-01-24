ea42ce806f4f9ce25ab4159affdad535298eaaf2
GeoffNderitu <geoffreynderitun@gmail.com>
2024-01-24 11:58:44 +0300

    Different Data types

--- variables.dart
+++ variables.dart
@@ -3,4 +3,16 @@ void main(){
 
    print(name);
 
+   var nambari = 23;
+
+  print("Integer: $nambari");
+
+  var decimal = 34.21;
+
+  print("Double: $decimal");
+
+  var chagua = false;
+
+  print("Boolean: $chagua"); 
+
 }
